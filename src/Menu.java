1337928243885868e5e4500bace67fa287ed2e3e
Asmaa-Abdullah <asmaa_abdullah71@yahoo.com>
2019-10-18 23:04:55 +0200

    Design main menu of project as well as add TaskItem class

---
+++ src/Menu.java
@@ -0,0 +1,102 @@
+import java.text.ParseException;
+import java.util.Scanner;
+
+public class Menu {
+    public Scanner input = new Scanner(System.in);
+    public int selection = 0;
+    ToDoList toDoList = new ToDoList();
+    //TaskItem taskItem = new TaskItem();
+
+    public void mainMenu() throws ParseException {
+
+        System.out.println("*********************************************************");
+        System.out.println("\n**       Welcome to the ToDo List Application       **\n");
+        System.out.println("*********************************************************");
+
+
+
+        printMenu();
+    }
+
+    public void printMenu() throws ParseException {
+        do {
+            System.out.println("\n1 - Show Task List ");
+            System.out.println("2 - Add New Task ");
+            System.out.println("3 - Edit Or Remove Task");
+            System.out.println("4 - Save and Quit");
+            System.out.print("\nSelect from the main menu:  ");
+
+            selection = input.nextInt();
+            switch (selection) {
+                case 1:
+                    showTaskList();
+                    break;
+                case 2:
+                    toDoList.addNewTask();
+                    break;
+                case 3:
+                    editTask();
+                    break;
+                case 4:
+                    toDoFileManager. writeToFile(fileName, toDoList.getList());
+                    System.out.println("\n ********  Thanks for using the application!  ******** ");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid selection. Please try again.");
+            }
+
+        } while (selection != 4);
+    }
+
+    public void showTaskList() throws ParseException {
+        do {
+            System.out.println("\n1 - All Task ");           // everything
+            System.out.println("2 - Sort by date ");        // everything but sorted ascending by date
+            System.out.println("3 - Sort by project");      // everything but sorted ascending by project name
+            System.out.println("4 - Back to main menu ");
+            System.out.print("\nSelect from the menu:  ");
+
+            selection = input.nextInt();
+            switch (selection) {
+                case 1:
+                    toDoList.showAll();
+                    break;
+                case 2:
+                    toDoList.showByDate();
+                    break;
+                case 3:
+                    toDoList.showByProject();
+                    break;
+                case 4:
+                    printMenu();
+                    break;
+                default:
+                    System.out.println("Invalid selection. Please try again.");
+            }
+        } while (selection != 4);
+    }
+
+    public void editTask() throws ParseException {
+        do {
+            System.out.println("\n1 - Update and Remove");
+            System.out.println("2 - Back to main menu ");
+            System.out.print("\nSelect from the menu:  ");
+
+            selection = input.nextInt();
+            switch (selection) {
+                case 1:
+                    System.out.println("\n");
+                    toDoList.showAll();
+                    toDoList.chooseTaskIndex();
+                    break;
+                case 2:
+                    printMenu();
+                    break;
+                default:
+                    System.out.println("Invalid selection. Please try again.");
+            }
+        } while (selection != 2);
+    }
+}
+
---
+++ src/TaskItem.java
@@ -0,0 +1,76 @@
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class TaskItem implements Comparable {
+
+    private String projectName;
+    private String taskName;
+    private LocalDate taskDate;
+    private String taskStatus;
+    private String taskDescription;
+
+    public TaskItem(){}
+
+    public TaskItem(String projectName, String taskName,
+                    LocalDate taskDate, String taskStatusString, String taskDescription)
+    {
+        this.projectName = projectName;
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskDate = taskDate;
+        this.taskStatus = taskStatus;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public LocalDate getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(LocalDate taskDate) {
+        this.taskDate = taskDate;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "   "+projectName + "     " + taskName + "     " + taskDate + "     " + taskStatus + "     "+  taskDescription  ;
+    }
+
+    @Override
+    public int compareTo(Object o)
+    {
+        TaskItem other = (TaskItem) o;
+        return this.getTaskDate().compareTo(other.getTaskDate());
+    }
+}
